5e8a2b6bfcd09e94e4959b6d68e953d1096d9488
jaredatandi <jaredatandi07@gmail.com>
2022-11-07 21:35:10 +0300

    Prototype the cocktail sort algo

--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ listint_t **swap_listint_left(listint_t **list, listint_t *node);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* ifndef SORT_H */

07ef9230ac846899d07b720d9e9abd7c2f9e02d0
jaredatandi <jaredatandi07@gmail.com>
2022-11-07 08:33:49 +0300

    Add the main header file

---
+++ sort.h
@@ -0,0 +1,25 @@
+#ifndef SORT_H
+#define SORT_H value
+
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: pointer to the previous element of the list
+ * @next: pointer to the next element of the list
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* ifndef SORT_H */
